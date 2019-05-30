6abe0912090928474959aac4598afc53b9a34f67
Mukul Kansal <kansalmukul@gmail.com>
2019-05-30 15:37:57 +0530

    Initial commit of baseline version

---
+++ src/main/java/com/hcl/springboot/rest/SpringBootRestfulMicroServiceApplication.java
@@ -0,0 +1,14 @@
+package com.hcl.springboot.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulMicroServiceApplication.class, args);
+	}
+
+}
+
